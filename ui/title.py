9c428d728c7383b10b0fa55e1759ed60509e022c
jnoah <johnnoahjarry16@gmail.com>
2026-05-22 14:09:03 +0800

    feat: Add version number to title screen.

--- ui/title.py
+++ ui/title.py
@@ -1,7 +1,7 @@
 def displayTitleScreen():
-  print('''[==========================]
- | Simple Dungeon Crawler |
-[==========================]
+  print('''[========================================]
+ | Simple Dungeon Crawler Version 1.0.1 |
+[========================================]
         
 [1] = Start Game
 [2] = Exit
